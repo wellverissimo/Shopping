875e56b9f8b3bd4f9f44863694eb2623d24b488b
WELLINGTON_SILVA <wellington_silva@hotmail.com>
2023-05-24 22:25:43 -0300

    deixando as páginas dinâmicas

--- Site/urls.py
+++ Site/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from Site import views
 
 urlpatterns = [
-    path('',views.index, name = 'index'),
-    path('produtos', views.produto_lista, name='produto_lista'),
-    path('produto', views.produto_detalhe, name='produtos_detalhe')
+    path('', views.index, name='index'),
+    path('sobre-a-empresa', views.institucional, name='institucional'),
+    path('contato', views.contato, name='contato'),
+    path('cadastro', views.cadastro, name='cadastro'),
+    path('produtos', views.produto_lista, name='produto_lista' ),
+    path('produtos/<int:id>' , views.produto_lista_por_id, name='produto_lista_por_id'),
+    path('produto/<int:id>', views.produto_detalhe, name='produto_detalhe')
 ]
--- Site/views.py
+++ Site/views.py
@@ -1,11 +1,62 @@
 from django.shortcuts import render
+from Site.models import Departamento, Produto
+
 
 # Create your views here.
-def index (request):
-    return render(request, 'index.html')
+def index(request):
+    departamentos = Departamento.objects.all()
+    context = {
+        "departamento": departamentos 
+    }
+
+    return render(request, 'index.html',context)
 
 def produto_lista(request):
-    return render(request,'produtos.html')
+    departamentos = Departamento.objects.all()
+    produtos = Produto.objects.all()
+    context = {
+        "departamento": departamentos,
+        "produtos":produtos,
+        'nome_categoria' : "Todos Produtos"
+    }
+    return render(request, 'produtos.html',context)
+
+def produto_lista_por_id(request,id):
+    departamentos = Departamento.objects.all()
+    produto_por_departamento = Produto.objects.filter(departamento_id = id)
+    categoria = departamentos.get(id = id).nome
+
+    context = {
+        "departamento": departamentos,
+        'produtos' : produto_por_departamento,
+        'nome_categoria': categoria
+    }
+    return render(request, 'produtos.html',context)
 
 def produto_detalhe(request):
-    return render(request, 'produto_detalhes.html')
+    departamentos = Departamento.objects.all()
+    context = {
+        "departamento": departamentos 
+    }
+    return render(request, 'produto_detalhes.html',context)
+
+def institucional(request):
+    departamentos = Departamento.objects.all()
+    context = {
+        "departamento": departamentos 
+    }
+    return render(request, 'empresa.html',context)
+
+def contato(request):
+    departamentos = Departamento.objects.all()
+    context = {
+        "departamento": departamentos 
+    }
+    return render(request, 'contato.html',context)
+
+def cadastro(request):
+    departamentos = Departamento.objects.all()
+    context = {
+        "departamento": departamentos 
+    }
+    return render(request, 'cadastro.html',context)
